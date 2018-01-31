cc28d103de353f28a6ae13ec0595be2853a896ed
Marc SUN <marc@Marcs-iMac.local>
2018-01-31 16:47:50 +0800

    fix

--- config/base.js
+++ config/base.js
@@ -1,10 +1,13 @@
-const ip = 'http://www.geonoon.net';
+const ip = 'www.geonoon.net';
 const appPort = 5005;
 const mongoPort = 27017;
 
 const baseConfig = {
-    baseURL: ip + ':' + appPort,
+    baseURL: 'http://' + ip + ':' + appPort,
     mongoConfig: 'mongodb://passport:passport@' + ip + ':' + mongoPort + '/passport'
 };
+
+console.log(baseConfig.baseURL);
+console.log(baseConfig.mongoConfig);
 
 module.exports = baseConfig;
